ad86a179bde405ab80072cab45e429791203285f
Einere <kjwsx23@gmail.com>
2025-07-28 17:33:37 +0900

    env: tanstack query 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./Route";
+import { queryClient } from "./queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
