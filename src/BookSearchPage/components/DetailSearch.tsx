947fdf06dc90ee06a567816ebeb0b2ae81d95184
Einere <kjwsx23@gmail.com>
2025-07-30 18:47:10 +0900

    fix: 상세 검색 시도 시, 팝오버가 닫히도록 수정

--- src/BookSearchPage/components/DetailSearch.tsx
+++ src/BookSearchPage/components/DetailSearch.tsx
@@ -15,32 +15,38 @@ export function DetailSearch(props: DetailSearchProps) {
     <Popover className="relative">
       <PopoverButton className="button-secondary">상세 검색</PopoverButton>
       <PopoverPanel anchor="bottom" className="bg-white p-8 shadow-md">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            setValue("query", "");
-            refetch();
-          }}
-          className="grid grid-cols-2 grid-rows-2 gap-4"
-        >
-          <select defaultValue="title" {...register("queryTarget")}>
-            <option value="title">제목</option>
-            <option value="isbn">ISBN</option>
-            <option value="person">저자</option>
-            <option value="publisher">출판사</option>
-          </select>
-          <input
-            type="search"
-            placeholder="검색어 입력"
-            {...register("detailQuery", {
-              onChange: () => setValue("query", ""),
-            })}
-          />
-          <button type="submit" className="button-primary col-span-full w-full">
-            검색하기
-          </button>
-        </form>
+        {({ close }) => (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              setValue("query", "");
+              refetch();
+              close();
+            }}
+            className="grid grid-cols-2 grid-rows-2 gap-4"
+          >
+            <select defaultValue="title" {...register("queryTarget")}>
+              <option value="title">제목</option>
+              <option value="isbn">ISBN</option>
+              <option value="person">저자</option>
+              <option value="publisher">출판사</option>
+            </select>
+            <input
+              type="search"
+              placeholder="검색어 입력"
+              {...register("detailQuery", {
+                onChange: () => setValue("query", ""),
+              })}
+            />
+            <button
+              type="submit"
+              className="button-primary col-span-full w-full"
+            >
+              검색하기
+            </button>
+          </form>
+        )}
       </PopoverPanel>
     </Popover>
   );
